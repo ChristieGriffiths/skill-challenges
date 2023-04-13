0e08984f625336ab99f7139ca6a865c9862f3146
Christie <christiegriffiths@Christies-MacBook-Air.local>
2023-04-13 16:41:30 +0100

    punctuation checker completed

--- phase_3/Design_program/lib/punctuation_checker.rb
+++ phase_3/Design_program/lib/punctuation_checker.rb
@@ -1,3 +1,6 @@
 def punctuation_checker(string)
-  false
+  return false if string == ""
+  first_letter = string[0]
+  starts_with_capital = first_letter.upcase == first_letter
+  starts_with_capital && string.end_with?(".", "?", "!") 
 end 
--- phase_3/Design_program/spec/punctuation_checker_spec.rb
+++ phase_3/Design_program/spec/punctuation_checker_spec.rb
@@ -5,8 +5,26 @@ RSpec.describe 'a method that verifies sentence opens and closes correctly' do
     result = punctuation_checker("")
     expect(result).to eq false
   end 
-  it "returns true when given any string" do 
-    result = punctuation_checker("hey there")
-    expect(result).to eq true 
-  end
+
+  it "returns false when fails to end in punctuation but starts correctly" do 
+    result = punctuation_checker("Hey there")
+  end 
+  
+  context "true when given a correctly capitalized and punctuated string." do 
+    it "returns true when given a string that starts with " do 
+      result = punctuation_checker("Hey there.")
+      expect(result).to eq true 
+    end
+
+    it "returns true when given a string that starts with " do 
+      result = punctuation_checker("Hey there?")
+      expect(result).to eq true 
+    end
+
+    it "returns true when given a string that starts with " do 
+      result = punctuation_checker("Hey there!")
+      expect(result).to eq true 
+    end
+  end 
+
 end 
